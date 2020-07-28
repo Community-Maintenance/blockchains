6fa964f746c9067a40377e4f2edd9acf707f09fc
Karl Semich <0xloem@gmail.com>
2020-07-27 20:06:08 -0400

    [DATALAD] Recorded changes

--- make-subrepo.bash
+++ make-subrepo.bash
@@ -6,3 +6,5 @@ then
 fi
 datalad create -d . -D "$2" "$1"
 datalad create-sibling-github -d "$1" --github-organization Community-Maintenance --github-login passwordisusernamebackwards --github-passwd sdrawkcabemanresusidrowssap blockchains-"$1"
+datalad siblings add -d "$1" -s bsv --no-annex-info --url bsv://L3zZQetoHkdMrsiEPtZEks1Ct98d56ZLxDBiatLpRBJA7Y1xELyB/blockchains-"$1".git
+
--- publish-updates.bash
+++ publish-updates.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 datalad save -u -d . -r
-for remote in github skynet
+for remote in github skynet bsv
 do
 	datalad publish --dataset . --missing inherit --recursive -J2 --to="$remote"
 done
