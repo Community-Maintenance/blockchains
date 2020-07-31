18ccaf51d071dd96bf5249dc8e96bf52d2fe24f1
Karl Semich <0xloem@gmail.com>
2020-07-31 10:08:50 -0400

    git-annex in user@localhost.localdomain:/media/mu-later/blockchains

--- publish-updates.bash
+++ publish-updates.bash
@@ -1,1 +1,7 @@
-.git/annex/objects/Km/Pv/SHA3_512E-s297--eb9b6b2f5e735d0ad16efc92fbd70a61cda09ec699b270c8c7a115e5d4a97e5809746763d4e1fa18e81c19b4bd6e0c81273b7a5cba971d054e2de8e64e3f7457.bash/SHA3_512E-s297--eb9b6b2f5e735d0ad16efc92fbd70a61cda09ec699b270c8c7a115e5d4a97e5809746763d4e1fa18e81c19b4bd6e0c81273b7a5cba971d054e2de8e64e3f7457.bash
+#!/usr/bin/env bash
+datalad save -u -d . -r
+git remote add github https://passwordisusernamebackwards:sdrawkcabemanresusidrowssap@github.com/Community-Maintenance/blockchains.git
+for remote in github skynet bsv
+do
+	datalad publish --dataset . --missing inherit --recursive -J2 --to="$remote"
+done
