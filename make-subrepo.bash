5f668be4d28f6ed0c66718155823051893287a4d
Karl Semich <0xloem@gmail.com>
2020-07-31 10:34:50 -0400

    git-annex in A public repository for use in sharing blockchain history; feel free to expand to other uses, too

--- make-subrepo.bash
+++ make-subrepo.bash
@@ -5,6 +5,7 @@ then
 	exit -1
 fi
 datalad create -d . -D "$2" "$1"
+pushd "$1" && git annex initremote skynet chunk=128MiB type=external encryption=none externaltype=siaskynet; popd
 datalad create-sibling-github -d "$1" --github-organization Community-Maintenance --github-login passwordisusernamebackwards --github-passwd sdrawkcabemanresusidrowssap blockchains-"$1"
 datalad siblings add -d "$1" -s bsv --no-annex-info --url bsv://L3zZQetoHkdMrsiEPtZEks1Ct98d56ZLxDBiatLpRBJA7Y1xELyB/blockchains-"$1".git
 
