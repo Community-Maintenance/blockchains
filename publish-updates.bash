ac6a5e9d433525b3c91d4a7a76dd990ed6a6fe21
Karl Semich <0xloem@gmail.com>
2020-07-31 10:45:57 -0400

    [DATALAD] Recorded changes

--- publish-updates.bash
+++ publish-updates.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 datalad save -u -d . -r
-datalad siblings configure -s bsv -d . --url bsv://1CQKe1veVPUaxpeYXeqAHnA4SJQpnbfZ2v/blockchains.git --pushurl bsv://L3zZQetoHkdMrsiEPtZEks1Ct98d56ZLxDBiatLpRBJA7Y1xELyB/blockchains.git --no-annex-info -r
-datalad siblings configure -s github -d . --url  https://github.com/Community-Maintenance/blockchains.git --pushurl https://passwordisusernamebackwards:sdrawkcabemanresusidrowssap@github.com/Community-Maintenance/blockchains.git --no-annex-info -r
+datalad siblings configure -s bsv -d . --url bsv://1CQKe1veVPUaxpeYXeqAHnA4SJQpnbfZ2v/%NAME.git --pushurl bsv://L3zZQetoHkdMrsiEPtZEks1Ct98d56ZLxDBiatLpRBJA7Y1xELyB/%NAME.git --no-annex-info
+datalad siblings configure -s github -d . --url  https://github.com/Community-Maintenance/%NAME.git --pushurl https://passwordisusernamebackwards:sdrawkcabemanresusidrowssap@github.com/Community-Maintenance/%NAME.git --no-annex-info 
 for remote in github skynet bsv
 do
 	datalad publish --dataset . --missing inherit --recursive -J2 --to="$remote"
