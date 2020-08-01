bc8273153b6cc1ba7ed32c4aa75ecfb2bfbb8024
Karl Semich <0xloem@gmail.com>
2020-08-01 04:53:35 -0400

    git-annex in A public repository for use in sharing blockchain history; feel free to expand to other uses, too

--- make-subrepo.bash
+++ make-subrepo.bash
@@ -8,5 +8,5 @@ datalad create -d . -D "$2" "$1"
 pushd "$1" && cp ../.gitattributes .gitattributes && git add .gitattributes && popd
 pushd "$1" && git annex initremote skynet chunk=128MiB type=external encryption=none externaltype=siaskynet; popd
 datalad create-sibling-github -d "$1" --github-organization Community-Maintenance --github-login passwordisusernamebackwards --github-passwd sdrawkcabemanresusidrowssap blockchains-"$1"
-datalad siblings add -d "$1" -s bsv --inherit
+datalad siblings add -d "$1" -s bsv --inherit --pushurl bsv://L3zZQetoHkdMrsiEPtZEks1Ct98d56ZLxDBiatLpRBJA7Y1xELyB/blockchains.git --url bsv://1CQKe1veVPUaxpeYXeqAHnA4SJQpnbfZ2v/blockchains.git
 
